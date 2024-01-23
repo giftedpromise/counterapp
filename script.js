e68328c3713a41ca06f063f02acefca69a7ce3e7
giftedpromise <giftedpromise1992@gmail.com>
2024-01-23 22:40:31 +0100

    counterjs

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
 let count = 0;
+console.log(count);
 
 function increment() {
   count += 1;
-  console.log(count);
+  countEl.innerText = count;
+}
+
+function save() {
+  let countStr = " " + count + " - ";
+  saveEl.innerText += countStr;
+  countEl.innerText = 0;
+  count = 0;
 }
